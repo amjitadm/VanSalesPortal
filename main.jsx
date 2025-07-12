4f550957daa10bf3567bba098278da5cf180131b
amjitadm <almajidjawad.qatar@gmail.com>
2025-07-12 08:55:50 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import VanSalesPortal from '/VanSalesPortal.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VanSalesPortal />
-  </React.StrictMode>
-);
