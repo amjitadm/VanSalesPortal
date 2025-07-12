f65e7dd1478b0b3689aff1bb4d037bc736c7c151
amjitadm <almajidjawad.qatar@gmail.com>
2025-07-12 08:26:41 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import VanSalesPortal from 'VanSalesPortal.jsx';
+import VanSalesPortal from '/VanSalesPortal.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
