595b807d5f7bf46500ea8168bd67dcfbed368a5b
amjitadm <almajidjawad.qatar@gmail.com>
2025-07-12 08:55:25 +0300

    Delete input.jsx

--- input.jsx
+++
@@ -1,9 +0,0 @@
-export const Input = ({ name, value, onChange, placeholder }) => (
-  <input
-    className="border p-2 rounded-xl"
-    name={name}
-    value={value}
-    onChange={onChange}
-    placeholder={placeholder}
-  />
-);
