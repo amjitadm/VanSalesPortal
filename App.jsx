32bea483711dcc77a67352f68bc4c7c2596f03be
amjitadm <almajidjawad.qatar@gmail.com>
2025-07-13 13:31:09 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -204,7 +204,7 @@ function VanSalesPortal() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 flex">
+    <center><div className="min-h-screen bg-gray-50 flex">
       {/* Sidebar */}
       <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0`}>
         <div className="flex items-center justify-between h-16 px-6 border-b border-gray-200">
@@ -355,7 +355,7 @@ function VanSalesPortal() {
           onClick={() => setSidebarOpen(false)}
         />
       )}
-    </div>
+    </div></center>
   );
 }
 
